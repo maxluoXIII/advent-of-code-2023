f5c22d3936b4712259a2c8315d8d84d65f3b9d64
Maxiwell Luo <maxluoxiii@gmail.com>
2023-12-01 14:40:15 -0600

    Complete day 1 part 2

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,6 +1,22 @@
 use regex::Regex;
 use std::fs;
 
+fn convert_to_digit(to_convert: &str) -> usize {
+    match to_convert {
+        "zero" => 0,
+        "one" => 1,
+        "two" => 2,
+        "three" => 3,
+        "four" => 4,
+        "five" => 5,
+        "six" => 6,
+        "seven" => 7,
+        "eight" => 8,
+        "nine" => 9,
+        digit => digit.parse::<usize>().unwrap(),
+    }
+}
+
 fn main() {
     let input = fs::read_to_string("data/day1-full.txt").expect("Could not read the input file");
     let mut calibration_sum = 0;
@@ -8,10 +24,17 @@ fn main() {
         Regex::new(r"one|two|three|four|five|six|seven|eight|nine|zero|[0-9]").unwrap();
     for line in input.lines() {
         if !line.is_empty() {
-            let digits = line.chars().filter(|c| char::is_numeric(*c));
-            let mut num_str: String = String::from(digits.clone().next().unwrap());
-            num_str.push(digits.last().unwrap());
-            calibration_sum += num_str.parse::<usize>().unwrap();
+            let first_match = num_pattern.find(line).unwrap();
+            let mut prev_match = first_match;
+            let mut last_match = num_pattern.find_at(line, prev_match.start() + 1);
+            while last_match.is_some() {
+                prev_match = last_match.unwrap();
+                last_match = num_pattern.find_at(line, prev_match.start() + 1);
+            }
+            let last_match = prev_match;
+            let calibration_value =
+                convert_to_digit(first_match.as_str()) * 10 + convert_to_digit(last_match.as_str());
+            calibration_sum += calibration_value;
         }
     }
 
